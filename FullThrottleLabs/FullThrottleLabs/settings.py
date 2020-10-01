9171e2f101e1e38e54ddc9754f02a793517ebd3f
Manish-Shetake <70906519+Manish-Shetake@users.noreply.github.com>
2020-10-01 16:43:23 +0530

    Updated allowed host

--- FullThrottleLabs/FullThrottleLabs/settings.py
+++ FullThrottleLabs/FullThrottleLabs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aw%k-y_844k@u7md-^zd5^qjvjy85qkxnk)abyou#c83=bn=7c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
